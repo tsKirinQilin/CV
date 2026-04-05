8774a46edddd6869cf6d6fff13cac7af8ed8d0c2
tsKirinQilin <tilek.sakyev.kirinqilin@gmail.com>
2026-04-06 01:35:38 +0600

    Deployment changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const climateRoutes = require("./routes/climateRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
